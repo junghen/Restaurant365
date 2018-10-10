cdc6ea9d8d93b3b6337950aa184cb8b5ab2c7083
junghen <43999864+junghen@users.noreply.github.com>
2018-10-09 18:56:56 -0700

    Code Exercise 1
    Create a simple String calculator with a method int Add(string numbers)
    The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
    Start with the simplest test case of an empty string and move to 1 and two numbers

--- StringCalculator/StringCalculatorTest/UnitTest1.cs
+++ StringCalculator/StringCalculatorTest/UnitTest1.cs
@@ -1,14 +1,55 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
 
 namespace StringCalculatorTest
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTestZeroToTwoNumbers
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestEmptyString()
         {
+            string numbers = "";
+            StringCalculator.StringCalculator sc = new StringCalculator.StringCalculator();
+            Assert.AreEqual(0, sc.Add(numbers));
+        }
+
+        [TestMethod]
+        public void TestZero()
+        {
+            string numbers = "0";
+            StringCalculator.StringCalculator sc = new StringCalculator.StringCalculator();
+            Assert.AreEqual(0, sc.Add(numbers));
+        }
+
+        [TestMethod]
+        public void TestOneNumber()
+        {
+            string numbers = "5";
+            StringCalculator.StringCalculator sc = new StringCalculator.StringCalculator();
+            Assert.AreEqual(5, sc.Add(numbers));
+        }
+        [TestMethod]
+        public void TestTwoNumbers()
+        {
+            string numbers = "1,5";
+            StringCalculator.StringCalculator sc = new StringCalculator.StringCalculator();
+            Assert.AreEqual(6, sc.Add(numbers));
+        }
+        [TestMethod]
+        public void TestThreeNumbers()
+        {
+            string numbers = "1,5,6";
+            StringCalculator.StringCalculator sc = new StringCalculator.StringCalculator();
+            Assert.AreEqual(-1, sc.Add(numbers));
+        }
+        [TestMethod]
+        public void TestException()
+        {
+            string numbers = "1000000000,10000000000";
+            StringCalculator.StringCalculator sc = new StringCalculator.StringCalculator();
+            Assert.AreEqual(-1, sc.Add(numbers));
         }
     }
 }
